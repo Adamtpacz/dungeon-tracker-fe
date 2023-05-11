859d0c484b215852a43e5199ef5d2a4d1158e0b7
Adamtpacz <adamtpacz@gmail.com>
2023-05-11 15:22:38 -0400

    Add monster search form to encounter details page

--- src/pages/Encounter-Show/index.js
+++ src/pages/Encounter-Show/index.js
@@ -3,8 +3,12 @@ export default function EncounterShow() {
 
 
     return (
-        <section>
+        <section className="flex flex-col items-center">
             <h1>Encounter Show Page</h1>
+            <form className="">
+                <input className="text-center m-2 border-2 border-neutral-950 p-1" placeholder="Monster Name"/>
+                <button className="bg-slate-400 m-2 border-2 border-neutral-950 p-1 w-20">Add</button>
+            </form>
         </section>
     )
 }
