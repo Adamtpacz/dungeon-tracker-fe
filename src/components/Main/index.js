f3b844e0fac86f1402dbe141cbe0a166dc622ef4
Adamtpacz <adamtpacz@gmail.com>
2023-05-11 12:02:54 -0400

    Stub out campaign edit route and page

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom"
 import CampaignIndex from "../../pages/Campaign-Index"
 import CampaignShow from "../../pages/Campaign-Show"
 import CampaignCreate from "../../pages/Campaign-Create"
+import CampaignUpdate from "../../pages/Campaign-Update"
 
 export default function Main() {
     return (
@@ -11,6 +12,7 @@ export default function Main() {
                 <Route path='/' element={ <CampaignIndex /> } />
                 <Route path='/campaign/:id' element={ <CampaignShow /> } />
                 <Route path='/campaign/new' element={ <CampaignCreate /> } />
+                <Route path='/campaign/:id/edit' element={ <CampaignUpdate /> } />
             </Routes>
         </main>
     )
---
+++ src/pages/Campaign-Update/index.js
@@ -0,0 +1,3 @@
+export default function CampaignUpdate() {
+    return <h1>Campaign Update Page</h1>
+}
