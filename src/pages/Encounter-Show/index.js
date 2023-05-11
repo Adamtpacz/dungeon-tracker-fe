7f074adb4d5c4a022d2a326e84a2dd8630d32332
Adamtpacz <adamtpacz@gmail.com>
2023-05-11 19:47:22 -0400

    Implement search functionality for DnD API monster data

--- src/pages/Encounter-Show/index.js
+++ src/pages/Encounter-Show/index.js
@@ -3,46 +3,52 @@ import { useState, useEffect } from 'react'
 export default function EncounterShow() {
 
     const [ monster, setMonster ] = useState({
-        index: "goblin"
+        index: ""
     })
     const [ isLoading, setIsLoading ] = useState(true)
 
-    async function handleRequest() {
-        try {
-            const apiResponse = await getMonster(monster.index)
-            setMonster(apiResponse)
-            console.log(monster)
-            setIsLoading(false)
-        } catch(err) {
-            console.log(err)
-        }
-    }
+    // async function handleRequest() {
+    //     try {
+    //         const apiResponse = await getMonster(monster.index)
+    //         setMonster(apiResponse)
+    //         console.log(monster)
+    //         setIsLoading(false)
+    //     } catch(err) {
+    //         console.log(err)
+    //     }
+    // }
 
+    
+    // useEffect(() => {
+    //     handleRequest()
+    // }, [isLoading])
+    
     async function handleSubmit(e) {
         e.preventDefault()
         try {
-            await getMonster(monster)
-            setMonster({
-                index: ""
-            })
+            setMonster({ ...monster, [e.target.name]: e.target.value })
+            const apiResponse = await getMonster(monster.index.toLowerCase())
+            console.log(apiResponse)
         } catch(err) {
             console.log(err)
         }
     }
-
-    useEffect(() => {
-        handleRequest()
-    }, [isLoading])
-
+    
+    function handleChange(e) {
+        setMonster({ ...monster, [e.target.name]: e.target.value })
+    }
+    
     const loaded = () => {
         return (
             <section className="flex flex-col items-center">
             <h1>Encounter Show Page</h1>
-            <form className="">
+            <form className="" onSubmit={handleSubmit}>
                 <input 
+                onChange={handleChange}
                 className="text-center m-2 border-2 border-neutral-950 p-1 rounded-lg" 
                 placeholder="Monster Name"
                 name="index"
+                value={monster.index}
                 />
                 <button className="bg-slate-400 m-2 border-2 border-neutral-950 p-1 w-20 rounded-lg">Add</button>
             </form>
@@ -55,8 +61,18 @@ export default function EncounterShow() {
     }
 
     return (
-        <section>
-            {isLoading ? loading() : loaded()}
+        <section className="flex flex-col items-center">
+            <h1>Encounter Show Page</h1>
+            <form className="" onSubmit={handleSubmit}>
+                <input 
+                onChange={handleChange}
+                className="text-center m-2 border-2 border-neutral-950 p-1 rounded-lg" 
+                placeholder="Monster Name"
+                name="index"
+                value={monster.index}
+                />
+                <button className="bg-slate-400 m-2 border-2 border-neutral-950 p-1 w-20 rounded-lg">Add</button>
+            </form>
         </section>
     )
 }
