226a94cc498029fe65f5ce87e9f3f5168181656b
Adamtpacz <adamtpacz@gmail.com>
2023-06-02 14:03:31 -0400

    Refactor campaign update page for mobile responsiveness

--- src/pages/Campaign-Update/index.js
+++ src/pages/Campaign-Update/index.js
@@ -71,30 +71,30 @@ export default function CampaignUpdate() {
 
     const loaded = () => {
         return (
-            <section>
+            <section className='flex flex-col items-center mt-12'>
                 <h1 className='text-3xl'>Edit details for {campaign.title}</h1>
-                <form className='flex flex-col items-center' onSubmit={handleSubmit}>
-                    <label className='text-lg'>Title</label>
+                <form className='flex flex-col items-center w-5/6' onSubmit={handleSubmit}>
+                    <label className='text-xl'>Title</label>
                     <input
-                        className='m-2 border-2 border-neutral-950 p-2 rounded-lg w-1/3 text-center'
+                        className='m-2 border-2 border-neutral-950 p-2 rounded-lg w-full text-center'
                         onChange={handleChange}
                         value={editForm.title}
                         name="title"
                         required
                         placeholder="Enter your campaign's title"
                     />
-                    <label className='text-lg'>Description</label>
+                    <label className='text-xl'>Description</label>
                     <input
-                        className='m-2 border-2 border-neutral-950 p-2 rounded-lg w-1/3 text-center'
+                        className='m-2 border-2 border-neutral-950 p-2 rounded-lg w-full text-center'
                         onChange={handleChange}
                         value={editForm.description}
                         type="textarea"
                         name="description"
                         placeholder="Description"
                     />
-                    <label className='text-lg'>Starting Level</label>
+                    <label className='text-xl'>Starting Level</label>
                     <input
-                        className='m-2 border-2 border-neutral-950 p-2 rounded-lg w-1/3 text-center'
+                        className='m-2 border-2 border-neutral-950 p-2 rounded-lg w-full text-center'
                         onChange={handleChange}
                         value={editForm.startLevel}
                         name="startLevel"
@@ -102,9 +102,9 @@ export default function CampaignUpdate() {
                         type="number"
                         placeholder="Starting Level"
                     />
-                    <label className='text-lg'>Ending Level</label>
+                    <label className='text-xl'>Ending Level</label>
                     <input
-                        className='m-2 border-2 border-neutral-950 p-2 rounded-lg w-1/3 text-center'
+                        className='m-2 border-2 border-neutral-950 p-2 rounded-lg w-full text-center'
                         onChange={handleChange}
                         value={editForm.endLevel}
                         name="endLevel"
@@ -112,27 +112,27 @@ export default function CampaignUpdate() {
                         type="number"
                         placeholder="Ending Level"
                     />
-                    <label className='text-lg'>Number of Players</label>
+                    <label className='text-xl'>Number of Players</label>
                     <input
-                        className='m-2 border-2 border-neutral-950 p-2 rounded-lg w-1/3 text-center'
+                        className='m-2 border-2 border-neutral-950 p-2 rounded-lg w-full text-center'
                         onChange={handleChange}
                         value={editForm.numOfPlayers}
                         name="numOfPlayers"
                         type="number"
                         placeholder="Numbers of Players"
                     />
-                    <label className='text-lg'>Campaign Image URL</label>
+                    <label className='text-xl'>Campaign Image URL</label>
                     <input
-                        className='m-2 border-2 border-neutral-950 p-2 rounded-lg w-1/3 text-center'
+                        className='m-2 border-2 border-neutral-950 p-2 rounded-lg w-full text-center'
                         onChange={handleChange}
                         value={editForm.image}
                         name="image"
                         placeholder="Campaign Image URL"
                     />
-                    <button className='bg-slate-400 m-2 border-2 border-neutral-950 p-1 w-32 rounded-lg hover:bg-slate-300'>Submit</button>
+                    <button className='bg-slate-400 m-2 border-2 border-neutral-950 p-1 w-1/2 h-16 rounded-lg hover:bg-slate-300 text-2xl font-bold'>Submit</button>
                 </form>
-                <div className='flex justify-center'>
-                    <button className='bg-slate-400 hover:bg-slate-300 m-2 border-2 border-neutral-950 rounded-lg p-1 w-32' onClick={handleDelete}>Delete</button>
+                <div className='flex justify-center w-5/6'>
+                    <button className='bg-slate-400 m-2 border-2 border-neutral-950 p-1 w-1/2 h-16 rounded-lg hover:bg-slate-300 text-2xl font-bold' onClick={handleDelete}>Delete</button>
                 </div>
             </section>
         )
